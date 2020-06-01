e78ac7ee4d0461b6fd524dd9d8317319ca9e521c
ChiHang <chi.hang@qq.com>
2020-06-01 13:54:30 +0800

    测试提交

--- src/main/java/com/chihang/HelloWorld.java
+++ src/main/java/com/chihang/HelloWorld.java
@@ -8,5 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloWorld {
     public static void main(String[] args) {
         SpringApplication.run(HelloWorld.class,args);
+        System.out.println("你好");
     }
 }

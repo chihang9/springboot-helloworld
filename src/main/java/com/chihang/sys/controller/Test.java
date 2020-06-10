991a5752d23483780fb3a4ed318e36312523717c
ChiHang <chi.hang@qq.com>
2020-06-10 12:46:21 +0800

    再次提交测试

--- src/main/java/com/chihang/sys/controller/Test.java
+++ src/main/java/com/chihang/sys/controller/Test.java
@@ -16,13 +16,6 @@ public class Test {
 		list.add(new UserInfo("1"));
 		list.add(new UserInfo("7"));
 		list.add(new UserInfo("2"));
-		list.add(new UserInfo("5312123213123312"));
-		list.add(new UserInfo("7123123213"));
-		list.add(new UserInfo("3213123132"));
-		list.add(new UserInfo("5"));
-		list.add(new UserInfo("4"));
-		list.add(new UserInfo("3试试水313123"));
-		list.add(new UserInfo("6"));
 		Collections.sort(list,(s1,s2)->s1.getUserName().compareTo(s2.getUserName()));
 		list.stream().skip(3).collect(Collectors.toList());
 		System.out.println(list.stream().skip(3).collect(Collectors.toList()));

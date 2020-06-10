8c1a2ad433987e917f89513297b8b7b8b19b8d6b
ChiHang <chi.hang@qq.com>
2020-06-10 12:00:20 +0800

    正常提交

--- src/main/java/com/chihang/sys/controller/Test.java
+++ src/main/java/com/chihang/sys/controller/Test.java
@@ -21,7 +21,7 @@ public class Test {
 		list.add(new UserInfo("3213123132"));
 		list.add(new UserInfo("5"));
 		list.add(new UserInfo("4"));
-		list.add(new UserInfo("3试试水"));
+		list.add(new UserInfo("3试试水313123"));
 		list.add(new UserInfo("6"));
 		Collections.sort(list,(s1,s2)->s1.getUserName().compareTo(s2.getUserName()));
 		list.stream().skip(3).collect(Collectors.toList());
